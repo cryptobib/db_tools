2dc23e9a3d6fb5385fc1fe1a3f724b1d3b0ef483
Fabrice Benhamouda <yfabrice@amazon.com>
2024-07-23 15:42:18 -0400

    Remove empty fields & linting
    This commit, combined with the associated commit in
    https://github.com/cryptobib/lib
    allows to remove empty fields from the generated bibfiles.

--- gen.py
+++ gen.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 """
 This script generates db/crypto.bib from
   db/abbrev0.bib
@@ -12,24 +12,24 @@ folders "lib" and "db"
 
 import sys
 import os
+
 scriptdir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(scriptdir, "..", "lib"))
 sys.path.append(os.path.join(scriptdir, "..", "db"))
 
-import mybibtex.parser
-import mybibtex.generator
-from confs_years import *
-
-import logging
-
-import header
-import config
 from config import *
+import config
+import header
+import logging
+from confs_years import *
+import mybibtex.generator
+import mybibtex.parser
 
 mybibtex.generator.config = config
 logging.basicConfig(level=logging.DEBUG)
 
-def gen_crypto_bib(db, confs_years, expand_crossrefs):
+
+def gen_crypto_bib(db, confs_years, expand_crossrefs: bool):
     if expand_crossrefs == False:
         outname = "db/crypto_crossref.bib"
     else:
@@ -37,8 +37,9 @@ def gen_crypto_bib(db, confs_years, expand_crossrefs):
 
     with open(outname, "w") as out:
         out.write(header.get_header(config, "gen.py", confs_years))
-    
-        mybibtex.generator.bibtex_gen(out, db, expand_crossrefs=expand_crossrefs, include_crossrefs=not expand_crossrefs)
+
+        mybibtex.generator.bibtex_gen(out, db, expand_crossrefs=expand_crossrefs,
+                                      include_crossrefs=not expand_crossrefs, remove_empty_fields=True)
 
         out.write("\n")
         out.write("\n")
@@ -47,8 +48,12 @@ def gen_crypto_bib(db, confs_years, expand_crossrefs):
             for line in fin:
                 out.write(line)
 
+
 def main():
     parser = mybibtex.parser.Parser()
+    # It's important to use abbrev0.bib for the parsing
+    # otherwise we may be removing fields that are empty for abbrev3.bib but not for abbrev0.bib
+    # as we are removing fields that are empty after macro expansion
     parser.parse_file("db/abbrev0.bib")
     parser.parse_file("db/crypto_db.bib")
     db = parser.parse_file("db/crypto_conf_list.bib")
@@ -57,6 +62,7 @@ def main():
 
     gen_crypto_bib(db, confs_years, True)
     gen_crypto_bib(db, confs_years, False)
+
 
 if __name__ == "__main__":
     main()
--- gen_abbrev.py
+++ gen_abbrev.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 """
 This script generates db/abbrev?.bib from db/abbrev.bibyml.
 This script needs to be run in the root folder containing the
@@ -20,6 +20,7 @@ from config import *
 
 logging.basicConfig(level=logging.DEBUG)
 
+
 def get_value(d, short=0):
     #: key to search for value (in the bibyml) depending on the wanted version short
     keys = {
@@ -37,10 +38,12 @@ def get_value(d, short=0):
                 return d[k][""]
     return None
 
+
 def gen(out, abbrev, short=0):
+    # we need to do a recursion in the tree defined by the yaml
     def write_abbrev(d, path=[]):
         val = get_value(d, short=short)
-        if val != None:
+        if val is not None:
             key = "".join(path)
             out.write("@string{{{} = {}{}}}\n".format(
                 key, 
@@ -57,11 +60,41 @@ def gen(out, abbrev, short=0):
 
     write_abbrev(abbrev)
 
+
+def check_abbrev(abbrev):
+    """
+    Sanity check that abbrev file
+    """
+
+    def check_abbrev0_non_empty_field_if_other_non_empty(d, path=[]):
+        """
+        Ensure that if non-abbrev0 fields are not empty,
+        abbrev0 field is not empty
+        """
+        vals = [get_value(d, short=s) for s in [0,1,2,3]]
+
+        if any(vals[i] is not None and vals[i] != "" for i in range(1,4)):
+            assert vals[0] is not None and vals[0] != "", \
+                "field '{}' is non-empty for one non-abbrev0 file but is empty in abbrev0".format("".join(path))
+
+        for k,v in d.items():
+            if k in ["", "@0", "@1", "@2", "@3"]:
+                continue
+            check_abbrev0_non_empty_field_if_other_non_empty(v, path+[k])
+
+
+    check_abbrev0_non_empty_field_if_other_non_empty(abbrev)
+
+
 def main():
     abbrev = bibyml.parse(open("db/abbrev.bibyml"))
+
+    check_abbrev(abbrev)
+
     for short in range(4):
         with open("db/abbrev{}.bib".format(short), "w") as out:
             gen(out, abbrev, short=short)
+
 
 if __name__ == "__main__":
     main()
